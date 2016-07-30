dd2a29fb9dec78178e62b7cff63371598bf7ba99
chemsed <desian19@yahoo.fr>
2016-07-30 12:53:17 -0400

    Changed blogging/blog/models.py
    I set the database layout for the blog application by following the "Creating model" section of 2nd part of the official django tutorial (source:https://docs.djangoproject.com/en/1.9/intro/tutorial02/). The main field of the database are the post text and the comment text. There can be other fields to add (unlike), so I let anybody else to complete that.

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# The blog will have posts and comments.
+# A relationship is defined with the foreignKey method. Then, 
+# many comments can be related to a blog post. 
+class Post(models.Model):
+	post_text = models.Charfield(max_length=40000)
+	pub_date = models.DateTimeField("date published")
+	like = models.IntegerField(default=0)
+	
+class Comment)models.Model):
+	post = models.ForeignKey(Question, on_delete=models.CASCADE)
+	comment_text = models.CharField(max_length=10000)
+	pub_date = models.DateTimeField("date published")
