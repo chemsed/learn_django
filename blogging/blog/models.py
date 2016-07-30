ab4ebf0ae8e57d6acd44845b3fa0773ff5ca3480
chemsed <desian19@yahoo.fr>
2016-07-30 13:19:03 -0400

    Created 0001_initial.py (database migration)
    Migration were created for new database layout and it was applied to the database.
    These commands were run:
    
    $ python manage.py makemigrations blog
    Migrations for 'blog':
      0001_initial.py:
        - Create model Comment
        - Create model Post
        - Add field post to comment
    
    $ python manage.py sqlmigrate blog 0001
    BEGIN;
    --
    -- Create model Comment
    --
    CREATE TABLE "blog_comment" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "c                                       omment_text" varchar(10000) NOT NULL, "pub_date" datetime NOT NULL);
    --
    -- Create model Post
    --
    CREATE TABLE "blog_post" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "post                                       _text" varchar(40000) NOT NULL, "pub_date" datetime NOT NULL, "like" integer NOT                                        NULL);
    --
    -- Add field post to comment
    --
    ALTER TABLE "blog_comment" RENAME TO "blog_comment__old";
    CREATE TABLE "blog_comment" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "c                                       omment_text" varchar(10000) NOT NULL, "pub_date" datetime NOT NULL, "post_id" in                                       teger NOT NULL REFERENCES "blog_post" ("id"));
    INSERT INTO "blog_comment" ("id", "post_id", "comment_text", "pub_date") SELECT                                        "id", NULL, "comment_text", "pub_date" FROM "blog_comment__old";
    DROP TABLE "blog_comment__old";
    CREATE INDEX "blog_comment_f3aa1999" ON "blog_comment" ("post_id");
    
    COMMIT;
    
    $ python manage.py check
    System check identified no issues (0 silenced).
    
    $ python manage.py migrate
    Operations to perform:
      Apply all migrations: blog, admin, sessions, contenttypes, auth
    Running migrations:
      Rendering model states... DONE
      Applying blog.0001_initial... OK

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # A relationship is defined with the foreignKey method. Then, 
 # many comments can be related to a blog post. 
 class Post(models.Model):
-	post_text = models.Charfield(max_length=40000)
+	post_text = models.CharField(max_length=40000)
 	pub_date = models.DateTimeField("date published")
 	like = models.IntegerField(default=0)
 	
-class Comment)models.Model):
-	post = models.ForeignKey(Question, on_delete=models.CASCADE)
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 	comment_text = models.CharField(max_length=10000)
 	pub_date = models.DateTimeField("date published")
