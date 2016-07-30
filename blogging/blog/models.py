555c1f07a6f67c515d85f44e02acf4c16065fbcc
chemsed <desian19@yahoo.fr>
2016-07-30 12:42:41 -0400

    Revert "Modified blogging/blog/models.py"
    This reverts commit abb76449010126f50c09257c0fee9b96f7a868fc.

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-# Blog post models
-class Post(models.Model):
-	post_text = models.CharField(max_length=40000)
-	pub_date = models.DateTimeField("date published")
-	like = models.IntegerField(default = 0)
-	
-class Comment(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
-	
+# Create your models here.
