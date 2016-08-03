3f31b702fcea2bc4b23f7c30d1dbc6a3ab4765c4
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2016-08-03 15:12:46 +0100

    Applied PEP8 formatting and changed incorrect line endings to LF (UNIX)

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 # The blog will have posts and comments.
-# A relationship is defined with the foreignKey method. Then, 
-# many comments can be related to a blog post. 
+# A relationship is defined with the foreignKey method. Then,
+# many comments can be related to a blog post.
+
+
 class Post(models.Model):
-	post_text = models.CharField(max_length=40000)
-	pub_date = models.DateTimeField("date published")
-	like = models.IntegerField(default=0)
-	
+    post_text = models.CharField(max_length=40000)
+    pub_date = models.DateTimeField("date published")
+    like = models.IntegerField(default=0)
+
+
 class Comment(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
-	comment_text = models.CharField(max_length=10000)
-	pub_date = models.DateTimeField("date published")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=10000)
+    pub_date = models.DateTimeField("date published")
