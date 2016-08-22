75450faa5a3a4aae7816f6d3a42df1490837cade
chemsed <desian19@yahoo.fr>
2016-08-22 14:05:34 -0400

    Change the representation of the Post and Comment objects

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -6,12 +6,18 @@ from django.db import models
 
 
 class Post(models.Model):
-    post_text = models.CharField(max_length=40000)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
     pub_date = models.DateTimeField("date published")
-    like = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.body
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=10000)
+    comment_text = models.TextField()
     pub_date = models.DateTimeField("date published")
+        def __str__(self):
+        return self.comment_text
