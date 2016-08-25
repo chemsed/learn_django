4da854b9545201aa626d2bb18d4908cfd29a61d8
chemsed <desian19@yahoo.fr>
2016-08-25 12:56:11 -0400

    Revert "Add data in the database with the interactive python shell and the django API"
    This reverts commit 8d3a3104d679fb0298b7591ee879b6d953217b1e.

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-import datetime
-from django.utils import timezone
 
 # The blog will have posts and comments.
 # A relationship is defined with the foreignKey method. Then,
@@ -10,21 +7,17 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField(default=None)
+    body = models.TextField()
     pub_date = models.DateTimeField("date published")
     likes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.title
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.body
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment_text = models.TextField()
     pub_date = models.DateTimeField("date published")
-
-    def __str__(self):
+        def __str__(self):
         return self.comment_text
