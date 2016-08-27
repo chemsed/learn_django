a79efd340cb91d6117eeea57cddd961b9b889de5
chemsed <desian19@yahoo.fr>
2016-08-26 22:36:53 -0400

    Updated codes so that it complies to PEP 8

--- blogging/blog/models.py
+++ blogging/blog/models.py
@@ -19,5 +19,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment_text = models.TextField()
     pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.comment_text
